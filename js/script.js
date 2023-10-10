ae5f1ba391a1e224965c8ce437f2ad69a524983e
Charlistio Aditirta Wijaya <charlistyo1@gmail.com>
2023-10-11 03:03:23 +0700

    second commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+function konversi() {
+    var celcius = parseFloat(document.getElementById("input").value);
+    if (isNaN(celcius)){
+        document.getElementById("error-input").innerText= "PLEASE INPUT YOUR VALUE FIRST !"
+
+    } else if (celcius == 0) {
+    var fahrenheit = (celcius * 9/5) + 32;
+    document.getElementById("result").innerHTML = fahrenheit.toFixed(2) + " Fahrenheit.";
+    document.getElementById("calculation").innerHTML = " CALCULATION PROCESS : " + celcius +"°C * (9/5) + 32 = " + fahrenheit.toFixed(2) +"°F";
+    document.getElementById("error-input").innerText=" "
+
+    } else {
+    document.getElementById("error-input").innerText=" "
+    var fahrenheit = (celcius * 9/5) + 32;
+    document.getElementById("result").innerHTML = fahrenheit.toFixed(2) + " Fahrenheit.";
+    document.getElementById("calculation").innerHTML = " CALCULATION PROCESS : " + celcius +"°C * (9/5) + 32 = " + fahrenheit.toFixed(2) +"°F";
+    }
+}
+
+function reset() {
+    document.getElementById("error-input").innerText=" "
+    document.getElementById("input").value = "";
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("calculation").innerHTML = "";
+}
+
+function reverse() {
+    var fahrenheit = parseFloat(document.getElementById("input").value);
+    if (isNaN(fahrenheit)) {
+        document.getElementById("error-input").innerText=" PLEASE INPUT YOUR VALUE FIRST ! "
+    } else if (fahrenheit == 0) {
+    var celcius = (fahrenheit - 32) * 5/9;
+    document.getElementById("result").innerHTML = celcius.toFixed(2) + " Celcius.";
+    document.getElementById("calculation").innerHTML = " CALCULATION PROCESS : (" + fahrenheit +"°F -32) * (9/5) = " + celcius.toFixed(2) +"°C";
+    document.getElementById("error-input").innerText=" "
+    } else {
+    document.getElementById("error-input").innerText=" "
+    var celcius = (fahrenheit - 32) * 5/9;
+    document.getElementById("result").innerHTML = celcius.toFixed(2) + " Celcius.";
+    document.getElementById("calculation").innerHTML = " CALCULATION PROCESS : (" + fahrenheit +"°F -32) * (9/5) = " + celcius.toFixed(2) +"°C";
+    }
+}
+
